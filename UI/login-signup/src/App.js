8c2092e9f0f92d2f3d26f4fbcd1c9ef0d426edad
pmd05 <duypm1750@ut.edu.vn>
2026-03-24 02:25:14 +0700

    remove unused dashboard folder

--- UI/login-signup/src/App.js
+++ UI/login-signup/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginSignup from './Components/LoginSignup/LoginSignup';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div>
-      <LoginSignup/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginSignup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
